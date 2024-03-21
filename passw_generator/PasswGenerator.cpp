839bce0592b062ab38cadebf5ccb686e8478c5d4
badgersky <szymon.pt4sznik@gmail.com>
2024-03-21 14:55:23 +0100

    passw generator abstract class

---
+++ passw_generator/PasswGenerator.cpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "PasswGenerator.h"
+
+string PasswGenerator::get_passw() {
+    return passw;
+}
+
+string PasswGenerator::generate_passw(unsigned size) {
+    return passw;
+}
---
+++ passw_generator/PasswGenerator.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class PasswGenerator {
+protected:
+    string lowercase = "qwertyuiopasdfghjklzxcvbnm";
+    string uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    string numbers = "1234567890";
+    string passw;
+public:
+    PasswGenerator() = default;
+
+    ~PasswGenerator() = default;
+
+    virtual string get_passw();
+
+    virtual string generate_passw(unsigned size);
+};
